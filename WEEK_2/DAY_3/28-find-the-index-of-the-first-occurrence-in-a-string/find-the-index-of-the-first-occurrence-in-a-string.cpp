e98e3f812ecaa5f421f0b10cb4ab6cbd4b51df7c
ANSHPAL18 <palansh880@gmail.com>
2026-06-10 11:28:12 +0530

    Update find-the-index-of-the-first-occurrence-in-a-string.cpp

--- WEEK_2/DAY_3/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ WEEK_2/DAY_3/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,24 +1,5 @@
 class Solution {
 public:
-  void getLPSLength(vector<int> &lps,string &s) {
-        int pre=0,suf=1;
-        while(suf<s.size()){
-            if(s[pre]==s[suf]){
-                lps[suf]=pre+1;
-                pre++,suf++;
-            }
-            else{
-                if(pre==0){
-                    lps[suf]=0;
-                    suf++;
-                }
-                else{
-                    pre=lps[pre-1];
-                }
-            }
-        }      
-    }
-    
     int strStr(string haystack, string needle) {
         int n=haystack.length();
         int m=needle.length();
